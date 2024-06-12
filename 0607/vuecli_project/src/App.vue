ee36c1cf5c966a82cc586c3580352575c9f8c0fa
Sky_Interest <841102500@qq.com>
2024-06-12 10:50:54 +0800

    十二、组件的使用

---
+++ 0607/vuecli_project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MyItem></MyItem>
+  </div>
+</template>
+
+<script>
+
+// 引入子组件
+
+import MyItem from '@/components/MyItem.vue'
+
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    components:{
+       MyItem
+    }
+  }
+</script>
+<style lang="less">
+
+</style>
---
+++ 0607/vuecli_project/src/components/MyItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box">
+        <h1 class="title">我是一个子组件的标题{{ msg }}</h1>
+        <img src="@/assets/logo.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '我是msg'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.box {
+    .title {
+        background: red;
+    }
+}
+</style>
